0a629177798b8174faac4ac41e2c7e770db2579d
SOUTHNINE\kang <kangnk5@gmail.com>
2017-03-27 15:36:06 +0900

    commnet

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });	
 });
-
+// /view에 있는 index.jade파일을 연결해준다. - 문법을 만들어주는데 그 형식이  jade
+// mvc 형식으로 다 쪼개져 있다. index.jade -> extends layout.jade ....
 module.exports = router;
